ea55896238bfbe87e8d5e9d4e90c92dafb8706fd
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-04-02 14:49:21 -0700

    configure pg

--- forms_server/forms_server/settings.py
+++ forms_server/forms_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y5w#pdz64c-_9ixpgmjvu)f28eiitbv#1!*og5b4m95%%utff$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'forms_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        # 'USER': config('USER'),
+        # 'PASSWORD': config('PASSWORD'),
+        'HOST': config('DATABASE_URL')
     }
 }
 
